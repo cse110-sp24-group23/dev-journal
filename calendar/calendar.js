d5a0beb39d1113efa61a85005b89681e9810f1da
brooke-tru <bmtruong@ucsd.edu>
2024-05-15 18:23:06 -0700

    Calendar.js Progress

---
+++ calendar/calendar.js
@@ -0,0 +1,17 @@
+
+/*
+Opens a new Daily Log page for the day that was clicked 
+Parameters:
+- link: receives a link of the page of the Daily Log associated with it 
+Returns:
+- None
+*/
+function dateLink(link){
+    //alert("You clicked on " + link.textContent)
+
+    //Link to daily log page for the specific day that was clicked 
+
+    //First time clicking a date: creates a new Daily Log page for it
+
+    //Revisiting a date: should pull up the existing Daily Log page for it 
+}

d1b5e41b565fb8e71a3e444c30b5ea56868c1053
Nick Guo <nicholasguoo@gmail.com>
2024-05-20 23:09:26 -0700

    navbar reusability and styling
    #19 added script to reuse navbar across pages and added to calendar page

---
+++ src/navbar/navbar.js
@@ -0,0 +1,32 @@
+/*
+This function loads the navbar component so it can be easily reused on multiple pages
+To use it add the following code to head:
+<script src="/src/navbar/navbar.js"></script>
+and following code to body:
+<div id="navbar"></div>
+Parameters:
+NONE
+Returns:
+- navbar
+*/
+function loadNavbar() {
+    fetch('/src/navbar/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+            document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="/src/navbar/navbar.css">');
+            
+            //Add class to determine active link
+            const currentPath = window.location.pathname;
+            const navLinks = document.querySelectorAll('.nav-link');
+            navLinks.forEach(link => {
+                console.log(link)
+                if (link.getAttribute('href') === currentPath) {
+                    link.classList.add('active');
+                }
+            });
+        })
+        .catch(error => console.error('Error loading navbar', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadNavbar);

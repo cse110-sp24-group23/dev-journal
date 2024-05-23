60383f3fb69296e6d012dd0e8d52a5effccd38a1
ravishende <ravi.sameer.shende@gmail.com>
2024-05-22 23:52:45 -0700

    added single comment to calendar.js for testing

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,7 +1,7 @@
 import { Calendar } from "./calendar-class.js";
 
 /*
-    Uses the Calendar class from ./calendar-class.js to populate the month view and show the next
+    Uses Calendar class from ./calendar-class.js to populate the month view and show the next
     or previous month when you click the `<` or `>` buttons on the calendar page.
     Also highlights the current day and grays out the rollover dates from the prev/next month.
     Parameters: None

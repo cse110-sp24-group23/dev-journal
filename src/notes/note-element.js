c0620f307c7042419540dfaeb04e07169b88d4e4
ravishende <ravi.sameer.shende@gmail.com>
2024-05-26 22:57:07 -0700

    Notes page in progress: cleaning code and adding functionality

--- src/notes/note-element.js
+++ src/notes/note-element.js
@@ -1,13 +1,12 @@
-
-class Note extends HTMLElement{
-    static get ObservedAttributes(){
-        return ['id', 'date', 'content', 'preview'];
+class Note extends HTMLElement {
+    static get ObservedAttributes() {
+        return ["id", "date", "content", "preview"];
     }
-    
-    constructor(){
+
+    constructor() {
         super();
-        this.classList.add('note');
-        this.shadow = this.attachShadow({mode: "closed"});
+        // this.classList.add('note');
+        this.shadow = this.attachShadow({ mode: "closed" });
         this.shadow.innerHTML = `
         
         <h4>${this.date}</h4>
@@ -19,7 +18,6 @@ class Note extends HTMLElement{
         this._date = null;
         this._content = null;
         this._preview = null;
-        
     }
     /*
     =======================
@@ -29,15 +27,15 @@ class Note extends HTMLElement{
     get id() {
         return this._id;
     }
-    
+
     get date() {
         return this._date;
     }
-    
+
     get content() {
         return this._content;
     }
-    
+
     get preview() {
         return this._preview;
     }
@@ -49,21 +47,21 @@ class Note extends HTMLElement{
     */
     set id(value) {
         this._id = value;
-        this.setAttribute('id', value);
+        this.setAttribute("id", value);
     }
-    
+
     set date(value) {
         this._date = value;
-        this.setAttribute('date', value);
+        this.setAttribute("date", value);
     }
-    
+
     set content(value) {
         this._content = value;
-        this.setAttribute('content', value);
+        this.setAttribute("content", value);
         const previewChars = 50;
         this._preview = this._content.slice(0, previewChars);
     }
-    
+
     /*
     Description: Change note attributes from oldValue to newValue
     Parameters: name, oldValue, newValue
@@ -72,33 +70,33 @@ class Note extends HTMLElement{
         - newValue: new content of attribute
     return: None
     */
-    attributeChangeCallback(name, oldValue, newValue){
-        if (oldValue === newValue){
+    attributeChangeCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) {
             return;
-        };
-        if (name === 'id'){
+        }
+        if (name === "id") {
             this.id = newValue;
-        };
+        }
         // if (name === 'title'){
         //     this.title = newValue;
         // };
-        if (name === 'date'){
+        if (name === "date") {
             this.date = newValue;
-        };
-        if (name === 'content'){
+        }
+        if (name === "content") {
             this.content = newValue;
-        };
-    };
+        }
+    }
     /*
     Description: Initialize values when new note is added.
     Parameters: None
     Return: None
     */
-    connectedCallback(){
-        this.id = this.getAttribute('id');
+    connectedCallback() {
+        this.id = this.getAttribute("id");
         // this.title = this.getAttribute('title');
-        this.date = this.getAttribute('date');
-        this.content = this.getAttribute('content');
-    };
-};
+        this.date = this.getAttribute("date");
+        this.content = this.getAttribute("content");
+    }
+}
 customElements.define("my-note", Note);
--- src/notes/notes.js
+++ src/notes/notes.js
@@ -1,80 +1,69 @@
 import { Record } from "../backend-storage/record-class.js";
 import { LocalStorageRecordsApi as RecordsStorage } from "../backend-storage/records-api.js";
 
+function loadFromStorage() {
+    const addNotes = document.querySelector(".add-notes");
+    const notesList = RecordsStorage.getAllRecords("note");
+    for (const note of notesList) {
+        let newNote = document.createElement("my-note");
+        newNote.id = note.id;
+        // newNote.title = note.title;
+        newNote.date = note.date;
+        newNote.content = note.field1;
+        addNotes.appendChild(newNote);
+    }
+}
 
-document.addEventListener("DOMContentLoaded", _editStorage);
-
-
-function _editStorage() {
-    // the textbox to enter notes in
-    const noteTextContent = document.getElementById("add-note-textbox");
+// submit to local storage
+function submitToStorage() {
     // the title of the note
     const noteTitle = document.getElementById("note-heading");
-    // submit button
-    const noteSubmitBtn = document.getElementById("noteSubmitBtn");
-    // delete button
-    const noteDeleteBtn = document.getElementById("noteDeleteBtn");
-    // thinking of creating a function where it generate an id for every note based on time note was created.
-      
+    // the textbox to enter notes in
+    const noteTextContent = document.getElementById("add-note-textbox");
+    const noteContent = noteTextContent.value;
+    const noteTitleContent = noteTitle.innerText;
+    const noteRecord = new Record("note", {
+        field1: noteContent,
+        title: noteTitleContent,
+    });
+    RecordsStorage.createRecord(noteRecord);
+}
 
-    // submit to local storage
-    function submitToStorage() {
-        
-        const noteContent = noteTextContent.value;
-        const noteTitleContent = noteTitle.value;
-        const noteRecord = new Record('note', {field1: noteContent, title: noteTitleContent});
-        RecordsStorage.createRecord(noteRecord);
-        
-    }
+// TODO: delete from local storage
+function deleteFromStorage(noteId) {
+    RecordsStorage.deleteRecord(noteId);
+}
 
-    function loadFromStorage() {
-        
-        const notesList = RecordsStorage.getAllRecords('note');
-        for (const note of notesList){
-            let newNote = document.createElement('my-note');
-            newNote.id = note.id;
-            // newNote.title = note.title;
-            newNote.date = note.date;
-            newNote.content = note.field1;
-        }
-        
-    }
-
-    // TODO: delete from local storage
-    function deleteFromStorage() {
-        localStorage.removeItem(noteId);
-    }
-
-    // load content autimatically
-    loadFromStorage();
-    // submit note content on click
-    // noteSubmitBtn.addEventListener("click", submitToStorage);
-    // delete note content on click
-    // noteDeleteBtn.addEventListener("click", deleteFromStorage);
+function _addNoteTextbox() {
+    const addNotes = document.querySelector(".add-notes");
+    const submitBtn = document.getElementById("submitNoteBtn");
+    const noteTextBox = document.createElement("div");
+    const noteHeading = document.createElement("h3");
+    const addNote = document.createElement("textarea");
+    const note = document.createElement("textarea");
+    const saveBtn = document.createElement("button");
+    saveBtn.innerText = "Save";
+    submitBtn.style.display = "block";
+    noteTextBox.className = "note";
+    noteTextBox.type = "text";
+    noteHeading.id = "note-heading";
+    noteHeading.setAttribute("contenteditable", "true");
+    addNote.id = "add-note-textbox";
+    addNotes.appendChild(noteHeading);
+    addNotes.appendChild(addNote);
+    addNotes.appendChild(saveBtn);
+    saveBtn.addEventListener("click", (event) => {
+        submitToStorage();
+        addNote.style.display = "none";
+    });
+    // notes.appendChild(noteTextBox).appendChild(noteHeading).appendChild(note);
+    // document.getElementById('add-note-textbox').style.display = 'block';
 }
 
 window.onload = function () {
-    const notes = document.querySelector('.notes');
-    const addNotes = document.querySelector('.add-notes');
-    document.getElementById('addNoteBtn').addEventListener('click', _addNoteTextbox);
-    window.addEventListener("DOMContentLoaded", _editStorage);
+    const notes = document.querySelector(".notes");
+    const addNoteBtn = document.getElementById("addNoteBtn");
+    addNoteBtn.addEventListener("click", _addNoteTextbox);
 
-    function _addNoteTextbox(){
-        let submitBtn = document.getElementById('submitNoteBtn');
-        let noteTextBox = document.createElement('div');
-        let noteHeading = document.createElement('h3');
-        let addNote = document.createElement('textarea');
-        let note = document.createElement('textarea');
-        submitBtn.style.display = 'block';
-        noteTextBox.className = 'note';
-        noteTextBox.type = 'text';
-        noteHeading.id = 'note-heading';
-        noteHeading.setAttribute('contenteditable', 'true');
-        addNote.id = 'add-note-textbox';
-        addNotes.appendChild(noteHeading);
-        addNotes.appendChild(addNote);
-        // notes.appendChild(noteTextBox).appendChild(noteHeading).appendChild(note);
-        
-        // document.getElementById('add-note-textbox').style.display = 'block';
-    };
+    window.addEventListener("DOMContentLoaded", loadFromStorage);
 };

4f5830b577848bd523e30204f3f9c552d3310dce
shambhavi-20 <shmittal@ucsd.edu>
2024-05-30 03:48:53 -0700

    #37 #8 added daily log functionality

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,7 +1,6 @@
 import { Calendar } from "./calendar-class.js";
-//import { populateDefaultLog } from "../dailyLog/dailyLog.js";
-//import { LocalStorageRecordsApi as RecordsStorage } from "../backend-storage/records-api.js";
-//import { Record } from "../backend-storage/record-class.js";
+import LocalStorageRecordsApi from "../backend-storage/records-api.js";
+import { Record } from "../backend-storage/record-class.js";
 
 /*
     Uses Calendar class from ./calendar-class.js to populate the month view and show the next
@@ -27,64 +26,29 @@ function calendarFunctionality(calendar) {
 }
 
 /*
-    TODO: change this to use localStorage once localStorage functionality is complete
-        - This whole function will likely be rewritten.
-    dateLink(): Opens a new Daily Log page for the day that was clicked 
-    Parameters: None
-    Returns: None
-*/
-/*async function dateLink(calendar) {
-    //Link to daily log page for the specific day that was clicked
-    //First time clicking a date: creates a new Daily Log page for it
-    try {
-        //path to the html boiler plate, please update if changed
-        const dailyLogPath = "../dailyLog/index.html";
-        // hello
-        const response = await fetch(dailyLogPath);
-        const htmlDailyLog = await response.text(); //get the html out of the response, if successful
-        const newDailyLog = document.createElement("link-for-date"); //creating a new element for daily log,
-        //later when the storage is up, we have to go through all of them and maybe match id or date?
-
-        // !! add classname when months are done to assist saving.
-        newDailyLog.innerHTML = htmlDailyLog;
-        const main = document.querySelector("main"); //reference to main element
-        main.append(newDailyLog); //add daily log to main
-        for (let i = 0; i < main.children.length - 1; i++) {
-            main.children[i].style.display = "none"; //hide other elements, only works when the current addedlog is the last one
-            //would not work for saved dates
-        }
-    } catch (error) {
-        console.error("Error fetching daily log:", error);
-    
-}}*/
-
-/*
     addClickToDays(): Adds event listeners to each date. 
     Parameters: None
     Returns: None
 */
-/*function addClickToDays(calendar) {
+function addClickToDays(calendar) {
     const calendarDays = document.querySelectorAll(".js-calendar-day");
     for (let day of calendarDays) {
         //day.innerHTML = "<a href= '../dailyLog/index.html'>helloo</a>";
         day.addEventListener("click", () => {
-            window.location.href = "../dailyLog/index.html";
-            alert("first");
             let record;
             const dateObject = calendar.getDateOfDayCell(day);
-            if (RecordsStorage.hasRecord(dateObject)) {
-                record = RecordsStorage.getRecordByDate(dateObject);
+            if (LocalStorageRecordsApi.hasRecordByDate(dateObject)) {
+                record = LocalStorageRecordsApi.getRecordByDate(dateObject);
             } else {
                 record = new Record("log", { date: dateObject });
             }
 
-            populateDefaultLog(record);
-            alert("second");
-        });
+            sessionStorage.setItem("current record", JSON.stringify(record));
 
-        //day.innerText = "helloo";
+            window.location.href = "../dailyLog/index.html";
+        });
     }
-}*/
+}
 
 window.onload = function () {
     // select relevant calendar elements
@@ -93,5 +57,5 @@ window.onload = function () {
     // instantiate Calendar class, passing in Heading and day cells since they will be edited
     const calendar = new Calendar(calendarHeading, calendarDayCells);
     calendarFunctionality(calendar);
-    //addClickToDays(calendar);
+    addClickToDays(calendar);
 };
--- src/dailyLog/dailyLog.js
+++ src/dailyLog/dailyLog.js
@@ -1,3 +1,5 @@
+import LocalStorageRecordsApi from "../backend-storage/records-api.js";
+
 function populateDefaultLog(record) {
     const updateH1 = document.querySelector("h1");
     updateH1.innerHTML = record.title;
@@ -15,4 +17,45 @@ function populateDefaultLog(record) {
     }
 }
 
-export { populateDefaultLog };
+function notesFunctionality(record) {
+    populateDefaultLog(record);
+    const submitButton = document.querySelector("#save-button");
+    const deleteButton = document.querySelector("#delete-button");
+    const date = new Date(record.date);
+    submitButton.addEventListener("click", () => {
+        if (!LocalStorageRecordsApi.hasRecordByDate(date)) {
+            LocalStorageRecordsApi.createRecord(record);
+        }
+        const updateField1 = document.querySelector("#done-today");
+        if (updateField1.value) {
+            record.field1 = updateField1.value;
+        }
+        const updateField2 = document.querySelector("#reflection");
+        if (updateField2.value) {
+            record.field2 = updateField2.value;
+        }
+        const updateHours = document.querySelector("#hours");
+        if (updateHours.value) {
+            record.hours = updateHours.value;
+        }
+        LocalStorageRecordsApi.updateRecord(record);
+        window.location.href = "../calendar/calendar.html";
+    });
+
+    deleteButton.addEventListener("click", () => {
+        if (LocalStorageRecordsApi.hasRecordByDate(date)) {
+            if (
+                window.confirm("Are you sure you want to delete this record?")
+            ) {
+                LocalStorageRecordsApi.deleteRecord(record.id);
+                window.location.href = "../calendar/calendar.html";
+            }
+        }
+    });
+}
+
+window.onload = function () {
+    const recordString = sessionStorage.getItem("current record");
+    const record = JSON.parse(recordString);
+    notesFunctionality(record);
+};

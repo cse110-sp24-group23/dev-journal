84bfb5319bf36b59b91cc8ffeb107d37781ce6c6
shambhavi-20 <shmittal@ucsd.edu>
2024-05-30 16:12:57 -0700

    #37 #19 fixed the active class name for nav bar

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -10,23 +10,46 @@ Returns:
 - navbar
 */
 function loadNavbar() {
-    fetch('/src/navbar/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-            document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="/src/navbar/navbar.css">');
-            
+    fetch("/src/navbar/navbar.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("navbar").innerHTML = data;
+            document.head.insertAdjacentHTML(
+                "beforeend",
+                '<link rel="stylesheet" href="/src/navbar/navbar.css">'
+            );
+
             //Add class to determine active link
             const currentPath = window.location.pathname;
-            const navLinks = document.querySelectorAll('.nav-link');
-            navLinks.forEach(link => {
-                console.log(link)
-                if (link.getAttribute('href') === currentPath) {
-                    link.classList.add('active');
+            const navLinks = document.querySelectorAll(".nav-link");
+            navLinks.forEach((link) => {
+                console.log(link);
+                if (link.getAttribute("href") === currentPath) {
+                    const recordString =
+                        sessionStorage.getItem("current record");
+                    const record = JSON.parse(recordString);
+                    if (!record) {
+                        link.classList.add("active");
+                    } else {
+                        const date = new Date(record.date);
+                        const today = new Date();
+                        today.setHours(0, 0, 0, 0);
+                        if (date.getTime() === today.getTime()) {
+                            link.classList.add("active");
+                        }
+                    }
                 }
             });
         })
-        .catch(error => console.error('Error loading navbar', error));
+        .catch((error) => console.error("Error loading navbar", error));
 }
 
-document.addEventListener('DOMContentLoaded', loadNavbar);
+function clearSessionStorage() {
+    const links = document.getElementsByClassName("js-nav-link");
+    for (const link of links) {
+        link.addEventListener("click", () => {
+            sessionStorage.removeItem("current record");
+        });
+    }
+}
+document.addEventListener("DOMContentLoaded", loadNavbar);

fc32e8fd43926137dae671c0303a634da435dfd3
Daniel <daniel.luoj@gmail.com>
2024-06-02 21:50:53 -0700

    implemented accomplishment page where it loads what is entered in dailylog

--- src/accomplishments/accomplishments.js
+++ src/accomplishments/accomplishments.js
@@ -1,5 +1,38 @@
-import LocalStorageAccomplishmentsApi from "../backend-storage/accomplishments-api";
+import LocalStorageAccomplishmentsApi from "../backend-storage/accomplishments-api.js";
+import { AccomplishmentsObj } from "../backend-storage/accomplishments-class.js";
 
 function loadAccomplishmentFromStorage(){
-    
+    const allAccomplishmentsObj = LocalStorageAccomplishmentsApi.getAllAccomplishmentsObj();
+    const dateCol = document.getElementById('date');
+    const accomplishmentCol = document.getElementById('accomplishment');
+    const tableBody = document.getElementById('tableBody');
+    for(const accomplishment of allAccomplishmentsObj){
+        // get the date from accomplishment obj
+        let dateOfAccomplishment = accomplishment.date;
+        let month ;
+        let day ;
+        let year ;  
+        let fullDate = dateOfAccomplishment.substring(0, 10);
+        // get the content from accomplishment obj
+        let contentOfAccomplishment = accomplishment.content;
+        for(let i = 0; i < contentOfAccomplishment.length; i++){
+            // create row of table
+            const row = document.createElement('tr');
+            // date of the accomplishemnt
+            const dateElement = document.createElement('td');
+            // content of the accomplishment
+            const accomplishmentElement = document.createElement('td');
+            // add the text
+            dateElement.innerHTML = fullDate;
+            accomplishmentElement.innerHTML = contentOfAccomplishment[i];
+            // add them to the table
+            tableBody.append(row);
+            row.append(dateElement);
+            row.append(accomplishmentElement);
+        }
+    }
+}
+
+window.onload = function(){
+    loadAccomplishmentFromStorage();
 }

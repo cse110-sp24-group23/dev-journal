421156ad1f7a6cbdfacc17a476edbda78511fcd7
David <davidanwyl99@gmail.com>
2024-06-04 22:33:34 -0700

    Updating process deployment url

--- test/end-to-end/e2e.test.js
+++ test/end-to-end/e2e.test.js
@@ -1,10 +1,8 @@
 import "dotenv/config";
 
-if (process.env.TEST_ENV === "LOCAL") {
-    const url = "'http://127.0.0.1:5500/index.html'"
-}
-else {
-    const url = process.env.DEPLOYMENT_URL;
+let url = "http://127.0.0.1:5500/index.html";
+if (process.env.DEPLOYMENT_URL) {
+    url = process.env.DEPLOYMENT_URL;
 }
 
 

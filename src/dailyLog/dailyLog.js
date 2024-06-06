5b4248fc5d133fe84680699a1fd1cd207ea380ac
shambhavi-20 <shmittal@ucsd.edu>
2024-06-06 12:00:12 -0700

    #121 added class names to save and delete buttons

--- src/dailyLog/dailyLog.js
+++ src/dailyLog/dailyLog.js
@@ -284,8 +284,8 @@ function updateContentButtons(newItem, text) {
 function logFunctionality(record) {
     //update content of the page
     populateDefaultLog(record);
-    const submitButton = document.querySelector("#save-button");
-    const deleteButton = document.querySelector("#delete-button");
+    const submitButton = document.querySelector(".js-save-button");
+    const deleteButton = document.querySelector(".js-delete-button");
 
     // Event listner for submit button
     submitButton.addEventListener("click", () => {

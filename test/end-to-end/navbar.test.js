f3be22ed7644871bd2147c895466aa4a77a35525
Benjamin Ryujin <55765860+BenjiRyujin@users.noreply.github.com>
2024-06-05 19:47:18 -0700

    #65 Finished nav-bar testing

--- test/end-to-end/navbar.test.js
+++ test/end-to-end/navbar.test.js
@@ -13,12 +13,68 @@ describe("Basic user flow for Navbar", () => {
         await page.goto(url); // Add page link
     });
 
-    // Visit the Calendar page
+    // Test for Calendar page
     test("Navigate to Calendar page", async () => {
-        // Click on Calendar button
-        page.click(".nav-list li:nth-child(1) a");
+        // Query calendar link in HTML
+        const calendarSelector = ".nav-list li:nth-child(1) a";
+        await page.waitForSelector(calendarSelector);
+
+        // Click on Calendar link in nav bar
+        await page.click(calendarSelector);
 
         // Assert if the URL matches the Calendar page URL
         expect(page.url()).toContain(`calendar/calendar.html`);
+    });
+
+    // Test for Today page
+    test("Navigate to Today page", async () => {
+        // Query today link in HTML
+        const todaySelector = ".nav-list li:nth-child(2) a";
+        await page.waitForSelector(todaySelector);
+
+        // Click on Today link in nav bar
+        await page.click(todaySelector);
+
+        // Assert if the URL matches the Today page URL
+        expect(page.url()).toContain("dailyLog/index.html");
+    });
+
+    // Test for Notes page
+    test("Navigate to Notes page", async () => {
+        // Query notes link in HTML
+        const notesSelector = ".nav-list li:nth-child(3) a";
+        await page.waitForSelector(notesSelector);
+
+        // Click on Notes link in nav bar
+        await page.click(notesSelector);
+
+        // Assert if the URL matches the Notes page URL
+        expect(page.url()).toContain("notes/notes.html");
+    });
+
+    // Test for Accomplishments page
+    test("Navigate to Accomplishments page", async () => {
+        // Query accomplishments link in HTML
+        const accomplishmentsSelector = ".nav-list li:nth-child(4) a";
+        await page.waitForSelector(accomplishmentsSelector);
+
+        // Click on Accomplishments link in nav bar
+        await page.click(accomplishmentsSelector);
+
+        // Assert if the URL matches the Accomplishments page URL
+        expect(page.url()).toContain("accomplishments/accomplishments.html");
+    });
+
+    // Test for Settings page
+    test("Navigate to Settings page", async () => {
+        // Query settings link in HTML
+        const settingsSelector = ".nav-list li:nth-child(5) a";
+        await page.waitForSelector(settingsSelector);
+
+        // Click on Settings link in nav bar
+        await page.click(settingsSelector);
+
+        // Assert if the URL matches the Settings page URL
+        expect(page.url()).toContain("settings/settings.html");
     });
 });

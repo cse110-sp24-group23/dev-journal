9788ff4e4382e6120114c6b1c1e68169068c4605
shambhavi-20 <shmittal@ucsd.edu>
2024-06-06 14:27:28 -0700

    #65 added a few tests for the calendar & Log page

--- test/end-to-end/daily-log.test.js
+++ test/end-to-end/daily-log.test.js
@@ -1,17 +1,92 @@
 import "dotenv/config";
 
-let url = "http://127.0.0.1:5500/index.html";
-
+//This should point to your local liveserver
+//Note that liveserver will be running at the root level, so requires /src/, while the hosted will be at the src/ level already.
+let url = "http://127.0.0.1:5501/src/calendar/calendar.html";
 if (process.env.DEPLOYMENT_URL) {
     url = process.env.DEPLOYMENT_URL;
 }
 
-describe("Daily Log E2E tests", () => {
+describe("Daily Log End-to-End Tests", () => {
     beforeAll(async () => {
         await page.goto(url); // Add page link
     });
-    it("Adding note", async () => {
-        // Your test logic goes here
-        expect(true).toBe(true);
+
+    test("Create New Daily Log", async () => {
+        // Query calendar link in HTML
+        const dateSelector = await page.$(".current-date");
+        await dateSelector.click();
+
+        // Wait for navigation to the new page
+        await page.waitForNavigation();
+
+        // Check if the URL contains 'dailyLog/index.html'
+        expect(page.url()).toContain(`dailyLog/index.html`);
+    });
+
+    // Test for Today page
+    test("Add text to different textareas", async () => {
+        // Wait for the textarea element to be visible
+
+        // Query the textarea element in HTML
+        const updateDoneToday = await page.$("#done-today");
+
+        // Define the text to be typed
+        const doneToday = ` - Checked emails and replied to urgent messages.
+        - Worked on the presentation for the client meeting.
+        - Worked on the user stories for a new feature.
+        - Reviewed code submissions from team members.`;
+
+        // Type the text into the textarea element
+        await updateDoneToday.type(doneToday);
+
+        await page.evaluate(
+            (updateDoneToday, doneToday) => {
+                updateDoneToday.value = doneToday;
+            },
+            updateDoneToday,
+            doneToday
+        );
+
+        // Retrieve the value from the textarea element
+        const addedText = await page.evaluate(
+            (updateDoneToday) => updateDoneToday.value,
+            updateDoneToday
+        );
+
+        expect(addedText).toBe(doneToday);
+    });
+
+    // Test for Today page
+    test("Add text to different textareas", async () => {
+        // Wait for the textarea element to be visible
+
+        // Query the textarea element in HTML
+        const updateDoneToday = await page.$("#done-today");
+
+        // Define the text to be typed
+        const doneToday = ` - Checked emails and replied to urgent messages.
+            - Worked on the presentation for the client meeting.
+            - Worked on the user stories for a new feature.
+            - Reviewed code submissions from team members.`;
+
+        // Type the text into the textarea element
+        await updateDoneToday.type(doneToday);
+
+        await page.evaluate(
+            (updateDoneToday, doneToday) => {
+                updateDoneToday.value = doneToday;
+            },
+            updateDoneToday,
+            doneToday
+        );
+
+        // Retrieve the value from the textarea element
+        const addedText = await page.evaluate(
+            (updateDoneToday) => updateDoneToday.value,
+            updateDoneToday
+        );
+
+        expect(addedText).toBe(doneToday);
     });
 });

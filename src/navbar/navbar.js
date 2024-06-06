bb2de01a436ecf0c7bee18a3204812d1190cbb2e
shambhavi-20 <shmittal@ucsd.edu>
2024-06-06 10:38:46 -0700

    #121 added comment to navBar.js

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -24,6 +24,8 @@ function loadNavbar() {
             const currentPath = window.location.pathname;
             const navLinks = document.querySelectorAll(".nav-link");
             navLinks.forEach((link) => {
+                // Split the href of the link and the current path by "/"
+                // and compare the last segments of both match
                 if (
                     link.getAttribute("href").split("/").pop() ===
                     currentPath.split("/").pop()

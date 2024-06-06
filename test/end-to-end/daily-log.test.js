5b146abc9c673dda25ae1e10cf72e62364a7772d
shambhavi-20 <shmittal@ucsd.edu>
2024-06-06 02:14:31 -0700

    #121 testing page.

--- test/end-to-end/daily-log.test.js
+++ test/end-to-end/daily-log.test.js
@@ -5,15 +5,13 @@ let url = "http://127.0.0.1:5500/index.html";
 if (process.env.DEPLOYMENT_URL) {
     url = process.env.DEPLOYMENT_URL;
 }
-describe("Daily Log E2E tests", () => {
-    // First, visit the lab 8 website
-    beforeAll(async () => {
-        let browser = await puppeteer.launch();
-        let page = await browser.newPage();
-        await page.goto(url);
-    });
 
+describe("Daily Log E2E tests", () => {
+    beforeAll(async () => {
+        await page.goto(url); // Add page link
+    });
     it("Adding note", async () => {
-        true.expect(true);
+        // Your test logic goes here
+        expect(true).toBe(true);
     });
 });

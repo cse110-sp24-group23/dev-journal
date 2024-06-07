3b7841cb964b23572a8ea46e7b453a3287d24947
shambhavi-20 <shmittal@ucsd.edu>
2024-06-06 17:12:22 -0700

    #65 tests for updating and saving a daily log

--- test/end-to-end/daily-log.test.js
+++ test/end-to-end/daily-log.test.js
@@ -64,12 +64,9 @@ describe("Daily Log End-to-End Tests", () => {
 
         // Define the text to be typed
         const doneToday = ` - Checked emails and replied to urgent messages.
-            - Worked on the presentation for the client meeting.
-            - Worked on the user stories for a new feature.
-            - Reviewed code submissions from team members.`;
-
-        // Type the text into the textarea element
-        await updateDoneToday.type(doneToday);
+        - Worked on the presentation for the client meeting.
+        - Worked on the user stories for a new feature.
+        - Reviewed code submissions from team members.`;
 
         await page.evaluate(
             (updateDoneToday, doneToday) => {
@@ -96,7 +93,6 @@ describe("Daily Log End-to-End Tests", () => {
         const hours = `7`;
 
         // Type the text into the textarea element
-        await updatehours.type(hours);
 
         await page.evaluate(
             (updatehours, hours) => {
@@ -122,8 +118,100 @@ describe("Daily Log End-to-End Tests", () => {
         const reflection = ` - Completed all planned tasks.
         - Prepare presentation for group meeting tomorrow.`;
 
+        await page.evaluate(
+            (updateReflection, reflection) => {
+                updateReflection.value = reflection;
+            },
+            updateReflection,
+            reflection
+        );
+        // Retrieve the value from the textarea element
+        const addedText = await page.evaluate(
+            (updateReflection) => updateReflection.value,
+            updateReflection
+        );
+
+        expect(addedText).toBe(reflection);
+    });
+
+    test("Save Log button", async () => {
         // Type the text into the textarea element
-        await updateReflection.type(reflection);
+        const saveButton = await page.$(".js-save-button");
+
+        await saveButton.click();
+        await page.waitForNavigation();
+
+        // Check if the URL contains 'dailyLog'
+        expect(page.url()).toContain(`calendar`);
+        const localStorageLength = await page.evaluate(() => {
+            return localStorage.length;
+        });
+
+        expect(localStorageLength).toBe(1);
+    });
+
+    test("Navigate to the page and populate record", async () => {
+        // Type the text into the textarea element
+        const dateSelector = await page.$(".current-date");
+        await dateSelector.click();
+
+        // Wait for navigation to the new page
+        await page.waitForNavigation();
+
+        // Check if the URL contains 'dailyLog'
+        expect(page.url()).toContain(`dailyLog`);
+
+        const updateDoneToday = await page.$("#done-today");
+
+        // Define the text to be typed
+        const doneToday = ` - Checked emails and replied to urgent messages.
+        - Worked on the presentation for the client meeting.
+        - Worked on the user stories for a new feature.
+        - Reviewed code submissions from team members.`;
+        const addedText = await page.evaluate(
+            (updateDoneToday) => updateDoneToday.value,
+            updateDoneToday
+        );
+
+        expect(addedText).toBe(doneToday);
+
+        // Query the textarea element in HTML
+        const updatehours = await page.$("#hours");
+
+        // Define the text to be typed
+        const hours = `7`;
+
+        // Retrieve the value from the textarea element
+        const addedHours = await page.evaluate(
+            (updatehours) => updatehours.value,
+            updatehours
+        );
+
+        expect(addedHours).toBe(hours);
+
+        // Query the textarea element in HTML
+        const updateReflection = await page.$("#reflection");
+
+        // Define the text to be typed
+        const reflection = ` - Completed all planned tasks.
+        - Prepare presentation for group meeting tomorrow.`;
+        // Retrieve the value from the textarea element
+        const addedReflection = await page.evaluate(
+            (updateReflection) => updateReflection.value,
+            updateReflection
+        );
+
+        expect(addedReflection).toBe(reflection);
+    });
+
+    test("Navigate to the page and populate record", async () => {
+        // Query the textarea element in HTML
+        const updateReflection = await page.$("#reflection");
+
+        // Define the text to be typed
+        const reflection = ` - Completed all planned tasks.
+        - Prepare presentation for group meeting tomorrow.
+        - Work on fixing bug.`;
 
         await page.evaluate(
             (updateReflection, reflection) => {
@@ -141,5 +229,75 @@ describe("Daily Log End-to-End Tests", () => {
         expect(addedText).toBe(reflection);
     });
 
-    test("Add text to reflection", async () => {});
+    test("Save Log button", async () => {
+        // Type the text into the textarea element
+        const saveButton = await page.$(".js-save-button");
+
+        await saveButton.click();
+        await page.waitForNavigation();
+
+        // Check if the URL contains 'dailyLog'
+        expect(page.url()).toContain(`calendar`);
+        const localStorageLength = await page.evaluate(() => {
+            return localStorage.length;
+        });
+
+        expect(localStorageLength).toBe(1);
+    });
+
+    test("Navigate to the page and populate record", async () => {
+        // Type the text into the textarea element
+        const todaySelector = ".nav-list li:nth-child(2) a";
+        await page.waitForSelector(todaySelector);
+
+        // Click on Today link in nav bar
+        await page.click(todaySelector);
+        await page.waitForNavigation();
+
+        // Check if the URL contains 'dailyLog'
+        expect(page.url()).toContain(`dailyLog`);
+
+        const updateDoneToday = await page.$("#done-today");
+
+        // Define the text to be typed
+        const doneToday = ` - Checked emails and replied to urgent messages.
+        - Worked on the presentation for the client meeting.
+        - Worked on the user stories for a new feature.
+        - Reviewed code submissions from team members.`;
+        const addedText = await page.evaluate(
+            (updateDoneToday) => updateDoneToday.value,
+            updateDoneToday
+        );
+
+        expect(addedText).toBe(doneToday);
+
+        // Query the textarea element in HTML
+        const updatehours = await page.$("#hours");
+
+        // Define the text to be typed
+        const hours = `7`;
+
+        // Retrieve the value from the textarea element
+        const addedHours = await page.evaluate(
+            (updatehours) => updatehours.value,
+            updatehours
+        );
+
+        expect(addedHours).toBe(hours);
+
+        // Query the textarea element in HTML
+        const updateReflection = await page.$("#reflection");
+
+        // Define the text to be typed
+        const reflection = ` - Completed all planned tasks.
+        - Prepare presentation for group meeting tomorrow.
+        - Work on fixing bug.`;
+        // Retrieve the value from the textarea element
+        const addedReflection = await page.evaluate(
+            (updateReflection) => updateReflection.value,
+            updateReflection
+        );
+
+        expect(addedReflection).toBe(reflection);
+    });
 });

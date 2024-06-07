3559e79306059f9ffd6a3cc1301e8f93a7adc2b5
ravishende <ravi.sameer.shende@gmail.com>
2024-06-06 18:08:23 -0700

    #110 added file to get/set markdown editing status

--- src/backend-storage/mde-mode-api.js
+++ src/backend-storage/mde-mode-api.js
@@ -1,7 +1,15 @@
-function setStatusMDE() {
-    return;
+/**
+ * Sets the status of whether to enable markdown editing
+ * @param {Boolean} statusMDE
+ */
+function setStatusMDE(statusMDE) {
+    localStorage.setItem("StatusMDE", statusMDE);
 }
 
+/**
+ * Gets the status of whether markdown editing is enabled
+ * @returns {Boolean} Whether or not markdown editing is enabled
+ */
 function getStatusMDE() {
-    return;
+    return localStorage.getItem("StatusMDE");
 }

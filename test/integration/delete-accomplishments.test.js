1a15064b7c4e0403a47314d736fbcd401337b1e7
shambhavi-20 <shmittal@ucsd.edu>
2024-06-07 17:42:48 -0700

    #169 delete accomplishment tests

---
+++ test/integration/delete-accomplishments.test.js
@@ -0,0 +1,27 @@
+import LocalStorageAccomplishmentsApi from "../../src/backend-storage/accomplishments-api.js";
+import { AccomplishmentsObj } from "../../src/backend-storage/accomplishments-class.js";
+import LocalStorageMock from "../../tools/localStorage-mock.js";
+
+global.localStorage = new LocalStorageMock();
+
+describe("Delete AccomplishmentsObj", () => {
+    it("Delete exsisting AccomplishmentsObj", () => {
+        const date = new Date();
+        const accomplishmentsObj = new AccomplishmentsObj(["test"], date);
+        LocalStorageAccomplishmentsApi.createAccomplishmentsObj(
+            accomplishmentsObj
+        );
+        LocalStorageAccomplishmentsApi.deleteAccomplishmentsObj(date);
+        const accomplishments =
+            LocalStorageAccomplishmentsApi.getAllAccomplishmentsObj();
+        expect(accomplishments.length).toBe(0);
+    });
+
+    it("AccomplishmentsObj not found", () => {
+        const date = new Date();
+        const errorMessage = `Could not delete accomplishments object, object not found for date:`;
+        expect(() => {
+            LocalStorageAccomplishmentsApi.deleteAccomplishmentsObj(date);
+        }).toThrow(errorMessage);
+    });
+});

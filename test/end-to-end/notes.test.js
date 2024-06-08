033a836d0c4fc48a4bf36130f8d539e9718ff99a
Benjamin Ryujin <55765860+BenjiRyujin@users.noreply.github.com>
2024-06-08 12:02:48 -0700

    #65 - Added TODO comments, removed commented code

--- test/end-to-end/notes.test.js
+++ test/end-to-end/notes.test.js
@@ -41,57 +41,12 @@ describe("Testing Notes Page functionality", () => {
     }, 10000); // Set timeout for 10 seconds
 
     /*
-    test("Update and view a note", async () => {
-        // Click "Add Note" button
-        await page.click("#add-note-btn");
-        // Wait for the note editor to appear
-        await page.waitForSelector("#note-editor");
-
-        // Type in the title and content
-        await page.type("#note-editor-title", "");
-        await page.type("#note-editor-content", "");
-
-        // Click the "Save" button
-        await page.click("editor-save-btn");
-
-        await page.waitForSelector("#note-element");
-        console.log("found note-element");
-
-        const noteElement = await page.$("note-element");
-        expect(noteElement).not.toBeNull();
-
-        await noteElement.click();
-        await page.waitForSelector("#note-editor");
-
-        // Check that the editor shows the correct title and content
-        const noteTitle = await page.$eval(
-            "#note-editor-title",
-            (el) => el.value
-        );
-        const noteContent = await page.$eval(
-            "#note-editor-content",
-            (el) => el.value
-        );
-
-        expect(noteTitle).toBe("");
-        expect(noteContent).toBe("");
-
-        // Update the title and content
-        await page.type("#note-editor-title", "Updated test note");
-        await page.type("#note-editor-content", "This is an updated test note");
-
-        // Click the "Save" button
-        await page.click("#editor-save-btn");
-
-        // Check that the note title and content has been updated
-        const updatedNoteTitle = await page.$eval("note-element", (el) =>
-            el.getAttribute("title")
-        );
-        const updatedNoteContent = await page.$eval("note-element", (el) =>
-            el.getAttribute("content")
-        );
-        expect(updatedNoteTitle).toBe("Updated test note");
-        expect(updatedNoteContent).toBe("This is an updated test note");
-    }, 100000);
+    TODO: Test viewing and updating of created notes
+    TODO: Test deletion of created notes
+    TODO: Test that notes are correctly populated after adding multiple
+     
+    COMMENTS: These tests were set aside due to the constraint of time, as well as not to hurt the existing automated testing framework
+    so close to deadline. All three tests deal with the manipulation of created notes, in which it was difficult for Puppeteer to correctly
+    access local storage on the hosted site where GitHub Actions were run. 
     */
 });

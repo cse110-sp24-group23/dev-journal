4954b279a41a3693dc0b65b7adc9e894e2b4c7e5
ravishende <ravi.sameer.shende@gmail.com>
2024-06-09 13:57:15 -0700

    #68 removed all mentions of password protection since we are not including it in dev this sprint

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,70 +1,5 @@
 import { setStatusMDE, getStatusMDE } from "../backend-storage/mde-mode-api.js";
-/**
- * Wait for the password to be submitted.
- * @param {Event} event - The submit event.
- */
-function setPassword() {
-    document
-        .getElementById("password-form")
-        .addEventListener("submit", async function (event) {
-            event.preventDefault();
-            const newPassword = document.getElementById("new-password").value;
-            const confirmPassword =
-                document.getElementById("confirm-password").value;
 
-            // Check if passwords match
-            // If they are, hash the password and store it in local storage.
-            if (newPassword === confirmPassword) {
-                const hashedPassword = await hashPassword(confirmPassword);
-                localStorage.setItem("storedHashedPassword", hashedPassword);
-                window.location.href = "../calendar/calendar.html";
-            } else {
-                const errorMessage = document.getElementById("error-message");
-                errorMessage.style.display = "block";
-            }
-        });
-}
-/**
- * Toggles visibility of the try again button when an error message is shown.
- */
-function toggleErrorDisplay() {
-    const tryAgainBtn = document.getElementById("try-again-button");
-    tryAgainBtn.addEventListener("click", function () {
-        const errorMessage = document.querySelector(".error");
-        errorMessage.style.display = "none";
-    });
-}
-/**
- * Changes the visibility of the password settings form.
- * If the password protection option is checked, then the form is visible.
- */
-function displayPasswordForm() {
-    document
-        .getElementById("toggle-password-form")
-        .addEventListener("change", function () {
-            const form = document.getElementById("password-form");
-            if (this.checked) {
-                form.classList.remove("hidden");
-            } else {
-                form.classList.add("hidden");
-            }
-        });
-}
-
-/**
- * Hashes the input password using SHA-256 algorithm.
- * @param {string} password - The input password from the user.
- * @returns {Promise<string>} The hashed input password.
- */
-async function hashPassword(password) {
-    const msgUint8 = new TextEncoder().encode(password);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-        .map((b) => b.toString(16).padStart(2, "0"))
-        .join("");
-    return hashHex;
-}
 /**
  * Sets the status of markdown editing based on the user's actions
  */
@@ -89,12 +24,12 @@ function updateStatusMDE() {
  * @name window.onload
  */
 window.onload = function () {
+    // Make sure the checkbox for markdown editing is checked/unchecked dependign on status
     const mdeCheckbox = document.querySelector(".js-mde-checkbox");
+    // It is checked by default, so if the status is false, make it unchecked
     if (!getStatusMDE()) {
         mdeCheckbox.checked = false;
     }
-    setPassword();
-    toggleErrorDisplay();
-    displayPasswordForm();
+    // Listen for clicks clicks on the checkbox to set the status of markdown editing
     updateStatusMDE();
 };
